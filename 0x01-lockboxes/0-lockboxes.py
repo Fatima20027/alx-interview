b0ef3729e19a0d951b28677438cab99f7b585dd3
Fatima20027 <fatima.hailou@icloud.com>
2024-10-10 18:54:09 +0100

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
-# method that determines if all the boxes can be opened.
+"""
+This method determines if all boxes can be opened, given an array of boxes.
+Each box contains keys that unlock other boxes.
 
-# using DFS
+The algorithm uses Depth-First Search (DFS) to explore the boxes.
+"""
+
 
 def canUnlockAll(boxes):
+    """
+    Determines whether all boxes can be unlocked starting from the first box.
+
+    The function starts from box 0 (always unlocked) and uses a stack
+    to keep track of which boxes are available to unlock.
+
+    Returns:
+    -------
+    bool
+        Returns True if all boxes can be unlocked, otherwise False.
+    """
+
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
