b24fc7e35412cecc5a491e712c1ba375be3f92e1
Fatima20027 <fatima.hailou@icloud.com>
2024-11-13 23:04:24 +0100

    task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+const url = 'https://swapi-api.alx-tools.com/api/films/3/';
+
+async function getData() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Could not fetch the resource');
+    }
+
+    const data = await response.json();
+    for (const characterUrl of data.characters) {
+      const charResponse = await fetch(characterUrl);
+
+      if (!charResponse.ok) {
+        throw new Error('Could not fetch character data');
+      }
+
+      const charData = await charResponse.json();
+      console.log(charData.name);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
