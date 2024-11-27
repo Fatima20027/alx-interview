330200b47e8eabac7154d5db3dd48e9e07190618
Fatima20027 <fatima.hailou@icloud.com>
2024-11-27 15:32:52 +0100

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """
+    Calculate the minimum number of coins required to achieve the given total.
+    """
+    if total <= 0:
+        return 0
+
+    # Initialize a list to store the minimum coins needed for each amount
+    combination = [float('inf')] * (total + 1)
+    combination[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            combination[i] = combination[i - coin] + 1
+    if combination[total] != float('inf'):
+        return combination[total]
+    else:
+        return -1
