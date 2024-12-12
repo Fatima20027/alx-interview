82f8a3954051ae4784e7187ac7e76f61dd876ce0
Fatima20027 <fatima.hailou@icloud.com>
2024-12-12 20:26:39 +0100

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -18,7 +18,7 @@ def isWinner(x, nums):
         prime[0] = prime[1] = False
         for i in range(2, int(n**0.5) + 1):
             if prime[i]:
-                for j in range(i * i, n + 1, n):
+                for j in range(i * i, n + 1, i):
                     prime[j] = False
         return prime
 
