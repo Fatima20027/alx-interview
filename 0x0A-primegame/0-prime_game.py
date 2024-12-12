933a9fd57956e682fa25cfec5d06fa02f217d999
Fatima20027 <fatima.hailou@icloud.com>
2024-12-12 20:14:04 +0100

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,6 +13,7 @@ def isWinner(x, nums):
         return None
 
     def sieve(n):
+        """Generate a list of prime numbers up to n using the Sieve of Eratosthenes."""
         prime = [True] * (n + 1)
         prime[0] = prime[1] = False
         for i in range(2, int(n**0.5) + 1):
